23de7d0b8b94d7f519687d2667e6b15c3f879f83
guisopo <solerguillermo@hotmail.com>
2020-11-17 11:51:33 +0100

    Add validation to request

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -11,7 +11,17 @@ class RegisterController extends Controller
         return view('auth.register');
     }
 
-    public function store() {
-        dd('aa');
+    public function store(Request $request) {
+        // Validate User
+        $this->validate($request, [
+            'name' => 'required|max255',
+            'username' => 'required',
+            'email' => 'required|email|max:255',
+            'password' => 'required|confirmed'
+        ]);
+
+        // Store User
+        // Sign user in
+        // Redirect user over
     }
 }
