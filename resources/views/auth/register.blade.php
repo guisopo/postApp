2d374892421a17ab149b0d18f4ffd93884094399
guisopo <solerguillermo@hotmail.com>
2020-11-17 12:02:01 +0100

    Add old value when there is a submition form error

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -10,7 +10,7 @@
       {{-- Name --}}
       <div class="mb-4">
         <label for="name" class="sr-only">Name</label>
-        <input type="text"  name="name" id="name" placeholder="Your name" class="bg-gray-100 border-2 w-full p-4 rounded-lg @error('name') border-red-500 @enderror" value ="">
+        <input type="text"  name="name" id="name" placeholder="Your name" class="bg-gray-100 border-2 w-full p-4 rounded-lg @error('name') border-red-500 @enderror" value ="{{ old('name') }}">
         @error('name')
         <div class="text-red-500 mt-2 text-small">
           {{ $message }}
@@ -20,7 +20,7 @@
       {{-- Username --}}
       <div class="mb-4">
         <label for="username" class="sr-only">Username</label>
-        <input type="text"  name="username" id="username" placeholder="Your username" class="bg-gray-100 border-2 w-full p-4 rounded-lg @error('name') border-red-500 @enderror" value ="">
+        <input type="text"  name="username" id="username" placeholder="Your username" class="bg-gray-100 border-2 w-full p-4 rounded-lg @error('name') border-red-500 @enderror" value ="{{ old('username') }}">
         @error('username')
         <div class="text-red-500 mt-2 text-small">
           {{ $message }}
@@ -30,7 +30,7 @@
       {{-- Email --}}
       <div class="mb-4">
         <label for="email" class="sr-only">Email</label>
-        <input type="text"  name="email" id="email" placeholder="Your email" class="bg-gray-100 border-2 w-full p-4 rounded-lg @error('name') border-red-500 @enderror" value ="">
+        <input type="text"  name="email" id="email" placeholder="Your email" class="bg-gray-100 border-2 w-full p-4 rounded-lg @error('name') border-red-500 @enderror" value ="{{ old('email') }}">
         @error('email')
         <div class="text-red-500 mt-2 text-small">
           {{ $message }}
