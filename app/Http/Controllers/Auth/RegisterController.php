b0f8c86452609a9fe16c27f76213249e663eb058
guisopo <solerguillermo@hotmail.com>
2020-11-17 12:11:00 +0100

    Store user after validatin form request

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -2,8 +2,10 @@
 
 namespace App\Http\Controllers\Auth;
 
-use App\Http\Controllers\Controller;
+use App\Models\User;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Hash;
 
 class RegisterController extends Controller
 {
@@ -21,7 +23,15 @@ class RegisterController extends Controller
         ]);
 
         // Store User
+        User::create([
+            'name' => $request->name,
+            'username' => $request->username,
+            'email' => $request->email,
+            'password' => Hash::make($request->password)
+        ]);
+
         // Sign user in
+
         // Redirect user over
     }
 }
