a79dd9ac339b23cdf6cd79eefe8ac57c37c4f979
guisopo <solerguillermo@hotmail.com>
2020-11-17 09:14:20 +0100

    Create nav

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -4,8 +4,37 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>postApp</title>
+  <link rel="stylesheet" href="{{asset('css/app.css')}}"/>
 </head>
 <body>
+  <nav class="p-6 bg-white flex justify-between">
+    <ul class="flex items-center">
+      <li>
+        <a href="" class="p-3">Home</a>
+      </li>
+      <li>
+        <a href="" class="p-3">Dashboard</a>
+      </li>
+      <li>
+        <a href="" class="p-3">Posts</a>
+      </li>
+    </ul>
+
+    <ul class="flex items-center">
+      <li>
+        <a href="" class="p-3">Guillermo Soler Poquet</a>
+      </li>
+      <li>
+        <a href="" class="p-3">Login</a>
+      </li>
+      <li>
+        <a href="" class="p-3">Register</a>
+      </li>
+      <li>
+        <a href="" class="p-3">Logout</a>
+      </li>
+    </ul>
+  </nav>
   @yield('content')
 </body>
 </html>
