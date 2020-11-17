1bc5c94096418b834e826b1b319e02d05645d7b6
guisopo <solerguillermo@hotmail.com>
2020-11-17 11:46:26 +0100

    Add csrf snippet for cross site validation

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -4,8 +4,9 @@
 
   <div class="flex justify-center">
     <div class="w-6/12 bg-white p-6 rounded-lg">
-      
+
     <form action="{{ route('register') }}" method="post">
+      @csrf
       {{-- Name --}}
       <div class="mb-4">
         <label for="name" class="sr-only">Name</label>
