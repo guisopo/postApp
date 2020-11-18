8533e15cbe4b0fefca952c64f4483187f218e8f7
guisopo <solerguillermo@hotmail.com>
2020-11-18 13:00:02 +0100

    Create LogoutController and its view and routes

--- routes/web.php
+++ routes/web.php
@@ -2,10 +2,17 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\DashboardController;
-use App\Http\Controllers\Auth\RegisterController;
 use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\Auth\LogoutController;
+use App\Http\Controllers\Auth\RegisterController;
+
+Route::get('/', function () {
+    return view('home');
+})->name('home');
 
 Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
+
+Route::post('/logout', [LogoutController::class, 'store'])->name('logout');
 
 Route::get('/login', [LoginController::class, 'index'])->name('login');
 Route::post('/login', [LoginController::class, 'store']);
@@ -13,6 +20,6 @@ Route::post('/login', [LoginController::class, 'store']);
 Route::get('/register', [RegisterController::class, 'index'])->name('register');
 Route::post('/register', [RegisterController::class, 'store']);
 
-Route::get('/', function () {
-    return view('post.index');
-});
+// Route::get('/', function () {
+//     return view('post.index');
+// });
