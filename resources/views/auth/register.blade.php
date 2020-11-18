4539cd673015161b6a23ed9993a3d4191cfaaba1
guisopo <solerguillermo@hotmail.com>
2020-11-18 10:52:42 +0100

    Fix indentation

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -12,9 +12,9 @@
         <label for="name" class="sr-only">Name</label>
         <input type="text"  name="name" id="name" placeholder="Your name" class="bg-gray-100 border-2 w-full p-4 rounded-lg @error('name') border-red-500 @enderror" value ="{{ old('name') }}">
         @error('name')
-        <div class="text-red-500 mt-2 text-small">
-          {{ $message }}
-        </div>
+          <div class="text-red-500 mt-2 text-small">
+            {{ $message }}
+          </div>
         @enderror
       </div>
       {{-- Username --}}
@@ -22,9 +22,9 @@
         <label for="username" class="sr-only">Username</label>
         <input type="text"  name="username" id="username" placeholder="Your username" class="bg-gray-100 border-2 w-full p-4 rounded-lg @error('name') border-red-500 @enderror" value ="{{ old('username') }}">
         @error('username')
-        <div class="text-red-500 mt-2 text-small">
-          {{ $message }}
-        </div>
+          <div class="text-red-500 mt-2 text-small">
+            {{ $message }}
+          </div>
         @enderror
       </div>
       {{-- Email --}}
@@ -32,9 +32,9 @@
         <label for="email" class="sr-only">Email</label>
         <input type="text"  name="email" id="email" placeholder="Your email" class="bg-gray-100 border-2 w-full p-4 rounded-lg @error('name') border-red-500 @enderror" value ="{{ old('email') }}">
         @error('email')
-        <div class="text-red-500 mt-2 text-small">
-          {{ $message }}
-        </div>
+          <div class="text-red-500 mt-2 text-small">
+            {{ $message }}
+          </div>
         @enderror
       </div>
       {{-- Password --}}
@@ -42,9 +42,9 @@
         <label for="password" class="sr-only">Password</label>
         <input type="password"  name="password" id="password" placeholder="Choose your password" class="bg-gray-100 border-2 w-full p-4 rounded-lg @error('name') border-red-500 @enderror" value ="">
         @error('password')
-        <div class="text-red-500 mt-2 text-small">
-          {{ $message }}
-        </div>
+          <div class="text-red-500 mt-2 text-small">
+            {{ $message }}
+          </div>
         @enderror
       </div>
       {{-- Confirm Password --}}
