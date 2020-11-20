3b4811b9a5cb8a1d96c1dfb5627573e99931f29d
guisopo <solerguillermo@hotmail.com>
2020-11-20 11:14:42 +0100

    Create user profile page

--- routes/web.php
+++ routes/web.php
@@ -7,10 +7,13 @@ use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\Auth\LoginController;
 use App\Http\Controllers\Auth\LogoutController;
 use App\Http\Controllers\Auth\RegisterController;
+use App\Http\Controllers\UserPostController;
 
 Route::get('/', function () {
     return view('home');
 })->name('home');
+
+Route::get('/users/{user:username}/posts', [UserPostController::class, 'index'])->name('users.posts');
 
 Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
 
